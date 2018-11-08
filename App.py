a00b675e1c9859a919bc47390819b50869acc661
vijjikomali <vijjikomali@gmail.com>
2018-11-08 15:41:32 -0500

    Add files via upload

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+#1. Import Flask
+
 import datetime as dt
 import pandas as pd
 from sqlalchemy.ext.automap import automap_base
@@ -21,13 +23,58 @@ Base.prepare(engine, reflect=True)
 
 # session = Session(bind=engine)
 
-
+# 2.reate an app
 app = Flask(__name__)
 
-
+# 3. Define static routes
 @app.route("/")
 def vij():
-        return "This is a test message !!"
-
+       
+        return (
+                "<h1>Hello, Everyone</h1><p>Welcome to the Climate Analysis of Hawaii!</p>"
+                
+                f"<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
+                f"<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
+                f"<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"    
+        )
+# 4. Define main behavior
 if __name__ == '__main__':
         app.run(debug=True) 
+
+# 5. Define static route - precipitation on Measurement Table
+@app.route("/api/v1.0/precipitation")
+def precipt():
+       
+        month_presp= session.query(Measurement.date).order_by(Measurement.id.desc()).first()
+        last_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+        rain = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= last_year).\
+        order_by(Measurement.date).all()
+        presp_df = pd.DataFrame(rain)
+
+        return (
+                f"<H3>Welcome to the Hawaii Climate Analysis API!<br/><br />"
+                f"<b> Routes of this area </b>"
+                f"<a href='/api/v1.0/precipitation'>/r of  Routes:<br/></b>"api/v1.0/precipitation</a><br/>"
+                f"<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
+                f"<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
+        )
+if __name__ == '__main__':
+        app.run(debug=True)
+
+@app.route("/api/v1.0/stations")
+def stations():
+       
+        list_st= session.query(Measurement.tobs).order_by(Measurement.tobs.desc()).first()
+        active_station = session.query(Measurement.station, func.count(Measurement.tobs)).group_by(Measurement.station).\
+        order_by(func.count(Measurement.tobs).desc()).all()
+        hitemp_obsrv= session.query(Measurement.station, Measurement.tobs).\
+        filter(Measurement.station  == 'USC00519281').\
+        filter(Measurement.date > last_year).all()
+
+        return (
+                
+        )
+
+if __name__ == '__main__':
+        app.run(debug=True)
